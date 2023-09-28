d6b256d3163dd3973e75d527c93553e689d020cf
Vlatko Juric <juricv78gmail.com>
2023-09-28 21:17:17 +0200

    js if else statements with logical operators

--- js/index.js
+++ js/index.js
@@ -6,7 +6,11 @@ button.addEventListener("click", () => {
   // Number(x) converts the string from the input to a number
   // Beware: input values are *always* strings!
   const age = Number(input.value);
-
+  if (age > 12 && age < 18) {
+    output.value = "You are a teen";
+  } else {
+    output.value = "You are not a teen";
+  }
   // Exercise:
   // Use conditions and the logical AND operator to write
   // "You are a teen." or "You are not a teen." into the output.
